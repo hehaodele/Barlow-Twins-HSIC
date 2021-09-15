6a7dac460b29e07265bf80e8ca63a5b56cf064aa
hehaodele <haohe@mit.edu>
2021-09-15 01:44:13 -0400

    plot

--- plot_curve.py
+++ plot_curve.py
@@ -5,7 +5,7 @@ import pandas as pd
 fn_list = os.listdir(result_path)
 print(fn_list)
 
-for fn in fn_list:
+for fn in filter(lambda x: x.startswith('Mbt+hsic') or x.startswith('0.00'), fn_list):
     df = pd.read_csv(f'{result_path}/{fn}')
     plt.plot(df['epoch'], df['test_acc@1'], label=fn)
 plt.legend()
